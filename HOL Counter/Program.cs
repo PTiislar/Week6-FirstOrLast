6f02694a1eedf7552492dd783b72d24d9527edaf
Pirgit Tiislar <ptiislar@tktk.ee>
2021-11-06 01:10:12 +0200

    Control Flow

---
+++ HOL Counter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HOL_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab konsoolis, mitu 'h', 'o' ja 'i' tähte on lauses "Hello World!"
+
+            string helloW = "Hello World";
+
+            string helloWLow = helloW.ToLower();
+
+            int stringLenght = helloWLow.Length;
+
+            int hCounter = 0;
+            int oCOunter = 0;
+            int lCounter = 0;
+
+            for (int i = 0; i < helloWLow.Length; i++)
+            {
+                if (helloWLow[i] == 'h')
+                {
+                    hCounter++;
+                }
+                else if (helloWLow[i] == 'o')
+                {
+                    oCOunter++;
+                }
+                else if (helloWLow[i] == 'l')
+                {
+                    lCounter++;
+                }
+            }
+            Console.WriteLine($"Lauses 'Hello World!' on {hCounter} h-t, {oCOunter} o-t, {lCounter} l-i."); ");
+
+        }
+    }
+}
